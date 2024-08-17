2ce60581ccd6bb9abe5c5ab8f3631d359ea40443
Forrest S <thewanderinwolves@gmail.com>
2024-08-16 18:15:22 -0700

    primitive types completed.

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,8 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a: Vec<i32> = (1..=100).collect();
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
@@ -13,3 +11,4 @@ fn main() {
         println!("Meh, I eat arrays like that for breakfast.");
     }
 }
+    
